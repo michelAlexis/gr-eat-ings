0760160f55eaf1232c8a01b325f736e830108e5f
michelAlexis <michel.alexis@outlook.com>
2022-06-06 15:24:17 +0200

    initial commit

---
+++ next-frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './pages/**/*.{html,js,jtx,ts,tsx}',
+    './components/**/*.{html,js,jtx,ts,tsx}',
+  ],
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
