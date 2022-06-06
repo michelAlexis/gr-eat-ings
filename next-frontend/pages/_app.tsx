fa4a5814865c9fd2e00d2edcfd0d95856e80c7cb
michelAlexis <michel.alexis@outlook.com>
2022-06-06 17:28:18 +0200

    feat: Add aws auth style and login message

--- next-frontend/pages/_app.tsx
+++ next-frontend/pages/_app.tsx
@@ -1,7 +1,17 @@
 import { AppProps } from 'next/app';
 import React from 'react';
 import '../styles/index.css';
+import '../node_modules/@aws-amplify/ui-react/dist/styles.css';
+import { Amplify } from '@aws-amplify/core';
+import awsconfig from '../../src/aws-exports';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+
+import { withAuthenticator } from '@aws-amplify/ui-react';
+
+Amplify.configure(awsconfig);
+
+export function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+
+export default withAuthenticator(MyApp as any);
--- next-frontend/pages/index.tsx
+++ next-frontend/pages/index.tsx
@@ -1,7 +1,20 @@
+import { Auth } from '@aws-amplify/auth';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 import Layout from '../components/Layout';
 
+// Amplify.configure(awsconfig);
+
 const IndexPage = () => {
+  const [user, setUser] = useState('');
+  useEffect(() => {
+    (async () => {
+      const current = await Auth.currentUserInfo();
+      console.log(current);
+      setUser(current.attributes.email);
+    })();
+  }, []);
+
   return (
     <Layout title="Home | Next.js + TypeScript Example">
       <h1>Hello Next.js 👋</h1>
@@ -10,8 +23,10 @@ const IndexPage = () => {
           <a>About</a>
         </Link>
       </p>
+      <p>Hello {user}</p>
     </Layout>
   );
 };
 
+// export default withAuthenticator(IndexPage);
 export default IndexPage;
