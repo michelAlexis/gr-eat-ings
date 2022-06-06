28a79b0f710381afdff771664c1756ce59ce3e07
michelAlexis <michel.alexis@outlook.com>
2022-06-06 16:18:12 +0200

    feat: Add AWS Amplify

--- next-frontend/tailwind.config.js
+++ next-frontend/tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   content: [
-    './pages/**/*.{html,js,jtx,ts,tsx}',
-    './components/**/*.{html,js,jtx,ts,tsx}',
-  ],
-  darkMode: 'media',
+      './pages/**/*.{html,js,jtx,ts,tsx}',
+      './components/**/*.{html,js,jtx,ts,tsx}',
+    ],
+    darkMode: 'media',
   theme: {
     extend: {},
   },
