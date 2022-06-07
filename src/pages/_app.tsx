91e5130b9f1653847d1a0622a4f4a76a26ed94a7
michelAlexis <michel.alexis@outlook.com>
2022-06-07 22:31:05 +0200

    fix: try using pnpm in amplify deploy ci

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { Amplify } from '@aws-amplify/core';
 import { AppProps } from 'next/app';
 import React from 'react';
-import awsconfig from '../../aws-exports';
+import awsconfig from '../aws-exports';
 // FIXME: There should be a better way to import the css
 import '../../node_modules/@aws-amplify/ui-react/dist/styles.css';
 import '../styles/index.css';
--- src/services/aws-client.ts
+++ src/services/aws-client.ts
@@ -1,5 +1,5 @@
 import { AWSAppSyncClient, AUTH_TYPE } from 'aws-appsync';
-import awsconfig from '../../aws-exports';
+import awsconfig from '../aws-exports';
 
 export const awsclient = new AWSAppSyncClient({
   //  FIXME: `disableOffline` is used because of "The keyPrefix reduxPersist: is already in use." error on the dev env.
