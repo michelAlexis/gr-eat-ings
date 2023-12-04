86bdc7c8d9e6971061f46cf7d67ccb2f639fd1c8
Alexis Michel <michel.alexis@outlook.com>
2023-12-04 12:13:24 +0100

    initial commit

---
+++ web/src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container h-full mx-auto flex justify-center items-center">
+    <div class="space-y-10 text-center flex flex-col items-center">
+        <h2 class="h2">Welcome to Skeleton.</h2>
+        <!-- Animated Logo -->
+        <div class="flex justify-center space-x-2">
+            <a
+                class="btn variant-filled"
+                href="https://skeleton.dev/"
+                target="_blank"
+                rel="noreferrer"
+            >
+                Launch Documentation
+            </a>
+        </div>
+        <div class="space-y-2">
+            <p>Try editing the following:</p>
+            <p><code class="code">Do you meen this file ?</code></p>
+            <p><code class="code">/src/routes/+layout.svelte</code></p>
+            <p><code class="code">/src/routes/+page.svelte</code></p>
+        </div>
+    </div>
+</div>
+
