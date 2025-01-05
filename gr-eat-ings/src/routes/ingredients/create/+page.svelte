57a2c7d9531657b72ec05f398bb2002c240549c1
Alexis Michel <michel.alexis@outlook.com>
2025-01-05 11:51:44 +0100

    chore: Put ingredient form into page

--- gr-eat-ings/src/routes/ingredients/create/+page.svelte
+++ gr-eat-ings/src/routes/ingredients/create/+page.svelte
@@ -1,11 +1,60 @@
 <script lang="ts">
 import type { PageData } from './$types.js';
-import IngredientForm from './ingredient-form.svelte';
+import * as Form from '$lib/components/ui/form';
+import { Input } from '$lib/components/ui/input';
+import SuperDebug, { superForm, arrayProxy } from 'sveltekit-superforms';
+import { zodClient } from 'sveltekit-superforms/adapters';
+import { createIngredientSchema } from './schema';
+import { fromStore } from 'svelte/store';
+import { toast } from 'svelte-sonner';
+import { Toaster } from '$lib/components/ui/sonner';
 
 type Props = { data: PageData };
 let { data }: Props = $props();
+
+const form = superForm(data.form, {
+    dataType: 'json',
+    validators: zodClient(createIngredientSchema),
+    onUpdated: ({ form }) => {
+        if (form.valid) {
+            reset();
+            toast.success('Ingredient has been created');
+        }
+    },
+});
+
+const { form: formData, errors, enhance, reset } = form;
+const { values: servings, valueErrors: servingsErrors } = arrayProxy(form, 'servings');
+const formDataState = fromStore(formData);
 </script>
 
 <div class="container">
-  <IngredientForm data={data.form} />
+  <Toaster richColors position="top-right" offset="60px" />
+  <form method="POST" use:enhance>
+    <Form.Field {form} name="label">
+      <Form.Control>
+        {#snippet children({ props })}
+          <Form.Label>Name</Form.Label>
+          <Input {...props} bind:value={$formData.label} />
+        {/snippet}
+      </Form.Control>
+      <Form.Description>This is the ingredient display name.</Form.Description>
+      <Form.FieldErrors />
+    </Form.Field>
+
+    <Form.Field {form} name="kcal">
+      <Form.Control>
+        {#snippet children({ props })}
+          <Form.Label>kcal</Form.Label>
+          <Input {...props} type="number" bind:value={$formData.kcal} />
+        {/snippet}
+      </Form.Control>
+      <Form.Description>Number of kcal per 100gr</Form.Description>
+      <Form.FieldErrors />
+    </Form.Field>
+
+    <Form.Button>Submit</Form.Button>
+  </form>
+
+  <SuperDebug data={formDataState} />
 </div>
